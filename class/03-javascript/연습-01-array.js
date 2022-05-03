4ca24ad92b6d4ab9345253caf7e0ea1fcf5324c0
Harry <osw0124@daum.net>
2022-05-03 15:15:02 +0900

    변수,상수,배열 연습

--- class/03-javascript/연습-01-array.js
+++ class/03-javascript/연습-01-array.js
@@ -1,9 +1,3 @@
-let students = ["길동", "철수", "영희", "준석"];
-console.log(students.length);
-console.log(students[0]);
-students.push("세웅");
-console.log(students);
-
 const classmate = ["오세웅", "배건희", "회민"];
 console.log(classmate[0]);
 classmate.push("코드캠프");

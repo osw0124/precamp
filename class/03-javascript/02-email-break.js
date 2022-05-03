9e286af51c64b1e549d44c4e0457bd3cc67ee417
Harry <osw0124@daum.net>
2022-05-03 15:46:09 +0900

    include, split, join 연습

---
+++ class/03-javascript/02-email-break.js
@@ -0,0 +1,84 @@
+// const email = 'codecamp@gmail.com'
+// undefined
+// email
+// 'codecamp@gmail.com'
+// email[0]
+// 'c'
+// let masking = []
+// undefined
+// masking
+// []
+// email.includes('@')
+// true
+// email.split('@')
+// (2) ['codecamp', 'gmail.com']
+// email.split('@')[0]
+// 'codecamp'
+// email.split('@')[0][0]
+// 'c'
+// email
+// 'codecamp@gmail.com'
+// email.includes('@')
+// true
+// email.split('@')[0][0]
+// 'c'
+// email.split('@')[0]
+// 'codecamp'
+// email.split('@')[1]
+// 'gmail.com'
+// let userMail = email.split('@')
+// undefined
+// userMail
+// (2) ['codecamp', 'gmail.com']
+// let userMail = email.split('@')[0]
+// undefined
+// let company = email.split('@')[1]
+// undefined
+// company
+// 'gmail.com'
+// let maskingMail = []
+// undefined
+// maskingMail
+// []
+// userMail[0]
+// 'c'
+// maskingMail.push(userMail[0])
+// 1
+// maskingMail
+// ['c']
+// maskingMail.push(userMail[1])
+// 2
+// maskingMail.push(userMail[2])
+// 3
+// maskingMail.push(userMail[3])
+// 4
+// maskingMail
+// (4) ['c', 'o', 'd', 'e']
+// masking.push('*')
+// 1
+// masking.push('*')
+// 2
+// masking.push('*')
+// 3
+// masking.push('*')
+// 4
+// maskingMail
+// (4) ['c', 'o', 'd', 'e']
+// maskingMail.push('*')
+// 5
+// maskingMail.push('*')
+// 6
+// maskingMail.push('*')
+// 7
+// maskingMail.push('*')
+// 8
+// maskingMail
+// (8) ['c', 'o', 'd', 'e', '*', '*', '*', '*']
+// maskingMail.join('')
+// 'code****'
+// maskingMail.join('') + '@' + company
+// 'code****@gmail.com'
+// let emailmasking = maskingMail.join('') + '@' + company
+// undefined
+// emailmasking
+// 'code****@gmail.com'

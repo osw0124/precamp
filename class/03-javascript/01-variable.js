b634860ee7055349ea8761b1fa4fe23f18c9f175
Harry <osw0124@daum.net>
2022-05-03 15:21:21 +0900

    약간 수정

--- class/03-javascript/01-variable.js
+++ class/03-javascript/01-variable.js
@@ -3,11 +3,3 @@ console.log(students.length);
 console.log(students[0]);
 students.push("세웅");
 console.log(students);
-
-const classmate = ["오세웅", "배건희", "회민"];
-console.log(classmate[0]);
-classmate.push("코드캠프");
-classmate.pop();
-console.log(classmate);
-console.log(classmate.length);
-console.log(classmate.includes("코드캠프"));

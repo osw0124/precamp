a168601eb577d50b36631678a65233d981c20054
오세웅 <48616771+osw0124@users.noreply.github.com>
2022-05-04 16:58:43 +0900

    Math 메소드 연습

---
+++ class/03-javascript/07-variable-math.js
@@ -0,0 +1,13 @@
+//0~1 사이의 숫자
+Math.random();
+
+//소숫점 아래 버림
+Math.floor();
+
+//문자열로 변환
+String();
+
+// 현재 문자열의 앞에 다른 문자열을 채워, 주어진 길이를 만족하는 새로운 문자열을 반환
+padStart();
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");

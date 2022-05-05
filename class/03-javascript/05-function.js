ee0ec131abc94d5bbdad0422e4b0ffdae97fe5ee
Harry <osw0124@daum.net>
2022-05-05 16:04:10 +0900

    랜덤 색 다른 방식 코드 추가

--- class/03-javascript/05-function.js
+++ class/03-javascript/05-function.js
@@ -9,6 +9,7 @@ const sendNumber = () => {
   let confirmNumber = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
   printBox = document.getElementById("printNumber");
 
-  printBox.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //   printBox.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  printBox.style.color = "#" + confirmNumber;
   printBox.innerText = confirmNumber;
 };

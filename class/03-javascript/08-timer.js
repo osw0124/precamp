ed53e50742505958b42b03b774a9e5c6d3597a0a
Harry <osw0124@daum.net>
2022-05-05 16:43:21 +0900

    setInterval을 이용한 타이머

--- class/03-javascript/08-timer.js
+++ class/03-javascript/08-timer.js
@@ -8,11 +8,21 @@
 //   console.log("1초가 지났습니다.");
 // }, 1000);
 
-let time = 10;
+// let time = 10;
 
+// setInterval(function () {
+//   if (time >= 0) {
+//     console.log(time);
+//     time = time - 1;
+//   }
+// }, 1000);
+
+let time = 180;
 setInterval(function () {
   if (time >= 0) {
-    console.log(time);
+    const min = String(Math.floor(time / 60)).padStart(2, "0");
+    const sec = String(time % 60).padStart(2, "0");
+    console.log(min + ":" + sec);
     time = time - 1;
   }
 }, 1000);

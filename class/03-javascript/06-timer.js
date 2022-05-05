223095dc9812b56cc2c88b792bc98c66bdb04f9b
Harry <osw0124@daum.net>
2022-05-05 17:50:26 +0900

    timer 완성

---
+++ class/03-javascript/06-timer.js
@@ -0,0 +1,59 @@
+// const sendNumber = () => {
+//   let confirmNumber = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+//   printBox = document.getElementById("printNumber");
+
+//   //   printBox.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//   printBox.style.color = "#" + confirmNumber;
+//   printBox.innerText = confirmNumber;
+// };
+
+// const timer = () => {
+//   let time = 180;
+//   setInterval(function () {
+//     if (time >= 0) {
+//       const min = String(Math.floor(time / 60)).padStart(2, "0");
+//       const sec = String(time % 60).padStart(2, "0");
+
+//       timerBox = document.getElementById("printTimer");
+
+//       timerBox.innerText = min + ":" + sec;
+//       time = time - 1;
+//     }
+//   }, 1000);
+// };
+
+let isStarted = false;
+
+const sendNumber = () => {
+  if (isStarted === false) {
+    isStarted = true;
+    const printBox = document.getElementById("printNumber");
+
+    let confirmNumber = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+
+    //   printBox.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    printBox.style.color = "#" + confirmNumber;
+    printBox.innerText = confirmNumber;
+
+    let time = 5;
+    let timerBox = document.getElementById("printTimer");
+    let confirmButton = document.getElementById("confirmButton");
+
+    let timer = setInterval(function () {
+      console.log("작동중");
+      if (time >= 0) {
+        const min = String(Math.floor(time / 60)).padStart(2, "0");
+        const sec = String(time % 60).padStart(2, "0");
+
+        console.log(min + ":" + sec);
+        timerBox.innerText = min + ":" + sec;
+
+        time = time - 1;
+      } else {
+        confirmButton.disabled = true;
+        isStarted = false;
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+};

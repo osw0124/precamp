8e6d3dbb34f8b395c2e6850a954ff85cff0e6750
Harry <osw0124@daum.net>
2022-05-05 15:13:16 +0900

    함수 연습을 위한 간단한 계산기 함수 만들기

---
+++ class/pre_cal-master/calculator.js
@@ -0,0 +1,18 @@
+// 힌트
+// input 태그의 값을 가져올 때
+// document.getElementById(" ").value 를 이용할 수 있습니다
+// 가져온 값의 타입은 문자열입니다. 숫자로 어떻게 변환해야 할까요?
+
+// 가이드
+// 1. input 태그의 값을 가져와 변수에 할당한다
+// 2. 결과를 담을 변수를 만들고, 연산 결과를 변수에 할당한다
+// 3. html의 결과 박스(id="result")의 내용을 결과값으로 바꿔준다
+
+function add() {
+  let input01 = parseInt(document.getElementById("a").value);
+  let input02 = parseInt(document.getElementById("b").value);
+
+  let addResult = input01 + input02;
+
+  document.getElementById("result").innerText = addResult;
+}
